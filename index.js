0abcf2273c5bc71529547f829820e38c79a96f58
VitorSantosLima <vitorlimacvo@gmail.com>
2025-08-12 17:02:26 -0300

    update

--- index.js
+++ index.js
@@ -1,33 +1,3 @@
-// subscribe to the IncomingCall event
-const sdk = VoxImplant.getInstance();
-sdk.on(VoxImplant.Events.IncomingCall, (e) => {
-
-    // add function to answer a call
-    document.getElementById("answer").onclick = () => {
-        e.call.answer();
-    };
-
-    // add function to decline a call
-    document.getElementById("decline").onclick = () => {
-        e.call.decline();
-    };
-
-    // subscribe to the necessary events
-    e.call.addEventListener(VoxImplant.CallEvents.Connected, () => {
-        console.log("Call connected!");
-        // you can process ending the call here, for example
-        document.getElementById("endcall").onclick = () => {
-            e.call.hangup();
-        };
-    });
-    e.call.addEventListener(VoxImplant.CallEvents.Disconnected, () => {
-        console.log("Call disconnected!");
-    });
-    e.call.addEventListener(VoxImplant.CallEvents.Failed, (e) => {
-        console.log("Call failed!");
-    });
-});
-
 function startcall() {
     // prepare settings
     const callSettings = {
